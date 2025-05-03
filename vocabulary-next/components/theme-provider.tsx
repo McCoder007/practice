96bbf887b8d23313ea928a537d89cc7c926becb4
McCoder007 <shawn@mcgrathplace.com>
2025-05-03 14:41:40 -0700

    fix: force light theme, disable system preference

--- vocabulary-next/components/theme-provider.tsx
+++ vocabulary-next/components/theme-provider.tsx
@@ -4,7 +4,12 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="light">
+    <NextThemesProvider 
+      attribute="class" 
+      defaultTheme="light"
+      enableSystem={false}
+      forcedTheme="light"
+    >
       {children}
     </NextThemesProvider>
   )
