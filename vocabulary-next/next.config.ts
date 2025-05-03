a1811880cad036f1d18dabc99fdc4be5b61004df
McCoder007 <shawn@mcgrathplace.com>
2025-05-03 05:40:58 +0000

    Deploying to gh-pages from @ McCoder007/practice@0d847614d9d3b0b890350136795a1130906567a1 🚀

--- vocabulary-next/next.config.ts
+++ vocabulary-next/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_GOOGLE_TTS_API_KEY: process.env.GOOGLE_TTS_API_KEY || '',
   },
-  basePath: '/practice/vocabulary-next-out',
+  basePath: '/practice/vocabulary-next',
   eslint: {
     ignoreDuringBuilds: true,
   },
