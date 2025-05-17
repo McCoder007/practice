3da585de96dd0d86f6cc42b833033fd321b935a8
Shawn McGrath <shawn@Shawns-14-MBP.localdomain>
2025-05-17 16:12:59 -0700

    Update Day 13 vocabulary with new words

---
+++ vocabulary-next/data/source/index.ts
@@ -0,0 +1,47 @@
+// Index file for all vocabulary source files
+
+import { day1 } from './day1';
+import { day2 } from './day2';
+import { day3 } from './day3';
+import { day4 } from './day4';
+import { day5 } from './day5';
+import { day6 } from './day6';
+import { day7 } from './day7';
+import { day8 } from './day8';
+import { day9 } from './day9';
+import { day10 } from './day10';
+import { day11 } from './day11';
+import { day12 } from './day12';
+import { day13 } from './day13';
+
+// Export as a single object for backward compatibility
+export const rawVocabularyData = {
+  day1,
+  day2,
+  day3,
+  day4,
+  day5,
+  day6,
+  day7,
+  day8,
+  day9,
+  day10,
+  day11,
+  day12,
+  day13,
+};
+
+// Export each day individually
+export { day1 };
+export { day2 };
+export { day3 };
+export { day4 };
+export { day5 };
+export { day6 };
+export { day7 };
+export { day8 };
+export { day9 };
+export { day10 };
+export { day11 };
+export { day12 };
+export { day13 };
